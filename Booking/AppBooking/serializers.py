249b5bdb011d63e4ea4bad898f33b4796bf78b38
Adam Łapiński <adam.lapinski@globallogic.com>
2025-03-31 11:34:45 +0200

    Delete Booking directory

--- Booking/AppBooking/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import Desk
-
-class DeskSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Desk
-        fields = '__all__'
