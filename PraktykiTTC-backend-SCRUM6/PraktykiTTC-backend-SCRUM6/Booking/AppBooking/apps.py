781e164b0406f85462c2ad59765da17638044cba
Adam Łapiński <adam.lapinski@globallogic.com>
2025-03-31 11:36:24 +0200

    Delete PraktykiTTC-backend-SCRUM6/PraktykiTTC-backend-SCRUM6/Booking directory

--- PraktykiTTC-backend-SCRUM6/PraktykiTTC-backend-SCRUM6/Booking/AppBooking/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class AppbookingConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'AppBooking'
--- PraktykiTTC-backend-SCRUM6/PraktykiTTC-backend-SCRUM6/Booking/AppBooking/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views import ListaKlientow
-
-urlpatterns = [
-    path('ListaKlientow/', ListaKlientow.as_view(), name='lista_klientow'),
-]
